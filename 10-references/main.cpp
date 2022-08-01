ff07b5938d0ee33efec911670ae220d69e41e2f8
Andre <33375224+Andree37@users.noreply.github.com>
2022-08-01 16:03:47 +0100

    some pointers

--- 10-references/main.cpp
+++ 10-references/main.cpp
@@ -15,4 +15,16 @@ int main() {
     print_me(str);
     print_me_but_no_changes(str);
     print_me_but_no_changes("5");
+
+    int x{5};
+
+    int* ptr1;       // garbage pointer
+    int* ptr2{};     // null pointer
+    int* ptr3{&x};   // pointer to x
+
+    ptr1 = nullptr;   // make ptr1 a null pointer
+    // dereferencing this pointer can be dangerous as it can create some undefined behavior
+    if (ptr1 == nullptr) {
+        std::cout << "null pointer lel" << std::endl;
+    }
 }
